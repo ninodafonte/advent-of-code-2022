594ba26780713ce3fd307f4ee312ae059cb1f0df
Nino Dafonte <nino@dafonte.dev>
2022-12-05 14:07:43 +0100

    feat(day5): walk to the park, new parsing idea that suck less

--- day5/puzzle1.js
+++ day5/puzzle1.js
@@ -1,6 +1,11 @@
 const input = require("./input");
 const NUM_OF_CRATES = 3;
-const POS_SPAN = 4;
+
+const mappedValues = {
+  ' ': '-',
+  '[': '#',
+  ']': ''
+}
 
 function initCratesContainer() {
   let data = [];
@@ -11,29 +16,34 @@ function initCratesContainer() {
   return data;
 }
 
-function containerToStacks(data) {
-  let stacks = initCratesContainer();
-  for (let i = 0; i < data.length; i++) {
-    for (let x = 0; x < data[i].length; x++) {
-      if (data[i][x] !== ' ') stacks[x].push(data[i][x]);
-    }
-  }
-
-  return stacks;
-}
-
-function parseCrates(cratesData) {
+function parseCrates() {
   let data = initCratesContainer();
+  let cratesData = input.split('\n')
+    .filter((elem, index) => index < NUM_OF_CRATES)
+    .reverse()
+    .map((elem) => {
+      return elem.replace(/ |\[|]/gi, (sym) => mappedValues[sym])
+        .replace('--', '#')
+        .replace(/-/gi, '')
+        .substring(1)
+    });
+
   for (let i = 0; i < cratesData.length; i++) {
-    for (let x = 1; x < NUM_OF_CRATES * POS_SPAN; x += POS_SPAN) {
-      if (cratesData[i][x] !== undefined) data[i].push(cratesData[i][x]);
+    let crateIndex = 0;
+    for (let x = 0; x < cratesData[i].length; x++) {
+      if (cratesData[i][x] === '#') { crateIndex++; continue; }
+      data[crateIndex].push(cratesData[i][x]);
     }
   }
 
-  return containerToStacks(data);
+  return data;
 }
 
-let cratesData = input.split('\n').filter((elem, index) => index < NUM_OF_CRATES).reverse();
-let crates = parseCrates(cratesData);
+function parseCommands() {
+
+}
+
+let crates = parseCrates();
+let commands = parseCommands();
 
 console.log(crates);

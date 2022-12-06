0db07580406291ad3c2288f1f20e1ff85a3cd944
Nino Dafonte <nino@dafonte.dev>
2022-12-06 10:59:24 +0100

    fix(day6): no need to have it back to array to count it

--- day6/puzzle2.js
+++ day6/puzzle2.js
@@ -5,7 +5,7 @@ const MESSAGE_SIZE = 14;
 let chunk = '';
 for (let i = 0; i < input.length; i++) {
   chunk = input.substring(i, i + MESSAGE_SIZE);
-  if ([...new Set(chunk.split(""))].join('').length === MESSAGE_SIZE) break;
+  if (new Set(chunk.split("")).size === MESSAGE_SIZE) break;
 }
 
 console.log(input.indexOf(chunk) + MESSAGE_SIZE)
